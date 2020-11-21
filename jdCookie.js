6e4f0fafdc648487179e769825a15070c379f23b
YEZZESDXHN <71679492+YEZZESDXHN@users.noreply.github.com>
2020-11-21 18:46:03 +0800

    Add files via upload

--- jdCookie.js
+++ jdCookie.js
@@ -4,7 +4,7 @@
 //此处填写京东账号cookie。
 //注：github action用户cookie填写到Settings-Secrets里面，新增JD_COOKIE，多个账号的cookie使用`&`隔开或者换行
 let CookieJDs = [
-  '',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
+  'pt_key=AAJfnNw6ADAEm9EKjY7rUOxAgeFEEdeT1k-dQJi5_CQaogr_lMpt1Z_nF0qgPbG0Bd2GsnJURO0; pt_pin=jd_751f17e61d4d0',//账号一ck,例:pt_key=XXX;pt_pin=XXX;
   '',//账号二ck,例:pt_key=XXX;pt_pin=XXX;如有更多,依次类推
 ]
 // 判断github action里面是否有京东ck
@@ -15,9 +15,14 @@ if (process.env.JD_COOKIE) {
   } else if (process.env.JD_COOKIE.indexOf('\n') > -1) {
     console.log(`您的cookie选择的是用换行隔开\n`)
     CookieJDs = process.env.JD_COOKIE.split('\n');
+  } else if (process.env.JD_COOKIE.indexOf('\\n') > -1) {
+    //环境变量兼容腾讯云和docker下\n会被转义成\\n
+    console.log(`您的cookie选择的是用换行隔开\\n`)
+    CookieJDs = process.env.JD_COOKIE.split('\\n');
   } else {
-    CookieJDs = process.env.JD_COOKIE.split();
+    CookieJDs = [process.env.JD_COOKIE];
   }
+  CookieJDs = [...new Set(CookieJDs)]
   console.log(`\n====================共有${CookieJDs.length}个京东账号Cookie=========\n`);
   console.log(`==================脚本执行- 北京时间(UTC+8)：${new Date(new Date().getTime() + new Date().getTimezoneOffset()*60*1000 + 8*60*60*1000).toLocaleString()}=====================\n`)
   // console.log(`\n==================脚本执行来自 github action=====================\n`)
